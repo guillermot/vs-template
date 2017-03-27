717d93ba3b5231e863ce71a22ef2e314850b3c5d
Guillermo Tamanaha <gtamanaha@nosis.interno>
2017-03-27 08:37:03 -0300

    first commit

---
+++ web-site/App_Start/RouteConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace web_site
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "login",
+                url: "login",
+                defaults: new { controller = "Home", action = "Login" }
+            );
+
+            routes.MapRoute(
+                name: "user-profile",
+                url: "user-profile",
+                defaults: new { controller = "Home", action = "UserProfile" }
+            );
+
+            routes.MapRoute(
+                name: "maps",
+                url: "maps",
+                defaults: new { controller = "Home", action = "Maps" }
+            );
+
+            routes.MapRoute(
+                name: "icons",
+                url: "icons",
+                defaults: new { controller = "Home", action = "Icons" }
+            );
+
+            routes.MapRoute(
+                name: "typography",
+                url: "typography",
+                defaults: new { controller = "Home", action = "Typography" }
+            );
+
+            routes.MapRoute(
+                name: "table-list",
+                url: "table-list",
+                defaults: new { controller = "Home", action = "TableList" }
+            );
+
+            routes.MapRoute(
+                name: "notifications",
+                url: "notifications",
+                defaults: new { controller = "Home", action = "TableList" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ web-site/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_site.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+
+        public ActionResult TableList()
+        {
+            return View();
+        }
+
+        public ActionResult Typography()
+        {
+            return View();
+        }
+
+        public ActionResult Icons()
+        {
+            return View();
+        }
+
+        public ActionResult Maps()
+        {
+            return View();
+        }
+
+        public ActionResult Notification()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
